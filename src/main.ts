b908da60900943fde85664d1671bb8c88f8f01c7
Etherio <50423290+etherio97@users.noreply.github.com>
2021-11-07 10:09:49 +0630

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { PORT } from "./config";
-import app from "./core/app";
+import { app } from "./core/app";
 
 app.listen(PORT, () =>
   console.log("Your app is running on http://localhost:%d", PORT)
